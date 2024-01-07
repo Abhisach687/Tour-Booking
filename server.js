a85c9a8f4c1df292f689395665b303d276071a7b
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-07 16:11:11 +0545

    database connection done

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const dotenv = require('dotenv');
+const sequelize = require('./db');
 
 dotenv.config({ path: './config.env' });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection successful!');
+  })
+  .catch(error => {
+    console.error('Unable to connect to the database:', error);
+  });
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });

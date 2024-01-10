0d4d71ec532b6106b2eb49f3ee3163c33aab34f5
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-10 15:59:40 +0545

    logging in users

--- models/userModel.js
+++ models/userModel.js
@@ -1,54 +1,66 @@
 const bcrypt = require('bcryptjs');
 const { sequelize, Sequelize } = require('../db');
 
-const User = sequelize.define('User', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  photo: {
-    type: Sequelize.STRING,
-    defaultValue: 'default.jpg'
-  },
-  role: {
-    type: Sequelize.STRING,
-    defaultValue: 'user'
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      len: [8, 100]
-    }
-  },
-  passwordConfirm: {
-    type: Sequelize.STRING,
-    allowNull: true,
-    validate: {
-      len: [8, 100],
-      isSameAsPassword(value) {
-        if (value !== this.password) {
-          throw new Error('Password confirmation must match password');
+const User = sequelize.define(
+  'User',
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    photo: {
+      type: Sequelize.STRING,
+      defaultValue: 'default.jpg'
+    },
+    role: {
+      type: Sequelize.STRING,
+      defaultValue: 'user'
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 100]
+      }
+    },
+    passwordConfirm: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      validate: {
+        len: [8, 100],
+        isSameAsPassword(value) {
+          if (value !== this.password) {
+            throw new Error('Password confirmation must match password');
+          }
         }
       }
+    },
+    passwordChangedAt: Sequelize.DATE,
+    passwordResetToken: Sequelize.STRING,
+    passwordResetExpires: Sequelize.DATE,
+    active: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true
     }
   },
-  passwordChangedAt: Sequelize.DATE,
-  passwordResetToken: Sequelize.STRING,
-  passwordResetExpires: Sequelize.DATE,
-  active: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: true
+  {
+    defaultScope: {
+      attributes: { exclude: ['password', 'passwordConfirm'] }
+    }
   }
-});
+);
+
+User.prototype.correctPassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 User.beforeCreate(async user => {
   if (!user.changed('password')) return;

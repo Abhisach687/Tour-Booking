c91f0354c79db3cceacfb6c1dd33dd451157780b
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-10 14:01:55 +0545

    user Model made

---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const { sequelize, Sequelize } = require('../db');
+
+const User = sequelize.define('User', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  photo: {
+    type: Sequelize.STRING,
+    defaultValue: 'default.jpg'
+  },
+  role: {
+    type: Sequelize.STRING,
+    defaultValue: 'user'
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [8, 100]
+    }
+  },
+  passwordConfirm: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [8, 100],
+      isSameAsPassword(value) {
+        if (value !== this.password) {
+          throw new Error('Password confirmation must match password');
+        }
+      }
+    }
+  },
+  passwordChangedAt: Sequelize.DATE,
+  passwordResetToken: Sequelize.STRING,
+  passwordResetExpires: Sequelize.DATE,
+  active: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true
+  }
+});
+
+module.exports = { User };

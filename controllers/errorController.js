5f4824678df23a600222008e2ca7124381d6978b
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-10 13:10:32 +0545

    server connection error handled

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,46 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack // Error stack trace
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    // This is the default value
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    if (err.name === 'SequelizeValidationError') {
+      err.message = err.errors.map(el => el.message).join('. ');
+    }
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      err.message = err.errors.map(el => el.message).join('. ');
+    }
+    sendErrorProd(err, res);
+  }
 };
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,7 @@ const { QueryTypes } = require('sequelize');
 const { sequelize } = require('../db');
 const { Tour } = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -11,185 +12,136 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    const features = new APIFeatures(Tour, req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Tour, req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    const tours = await features.execute();
-    // Send response
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: { tours }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'error',
-      message: err.message
-    });
-  }
-};
+  const tours = await features.execute();
+  // Send response
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours }
+  });
+});
 
-exports.getTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const tour = await Tour.findByPk(id);
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const tour = await Tour.findByPk(id);
 
-    if (!tour) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Tour not found'
-      });
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: { tour }
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error'
-    });
-  }
-};
-
-exports.createTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body);
-
-    res.status(201).json({
-      status: 'success',
-      data: { tour: newTour }
-    });
-  } catch (err) {
-    res.status(400).json({
+  if (!tour) {
+    return res.status(404).json({
       status: 'fail',
-      message: err
+      message: 'Tour not found'
     });
   }
-};
 
-exports.updateTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const tour = await Tour.findByPk(id);
+  res.status(200).json({
+    status: 'success',
+    data: { tour }
+  });
+});
 
-    if (!tour) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Tour not found'
-      });
-    }
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await Tour.create(req.body);
 
-    await tour.update(req.body, { validator: true }); // Set validator to true
+  res.status(201).json({
+    status: 'success',
+    data: { tour: newTour }
+  });
+});
 
-    res.status(200).json({
-      status: 'success',
-      data: { tour }
-    });
-  } catch (err) {
-    res.status(400).json({
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const tour = await Tour.findByPk(id);
+
+  if (!tour) {
+    return res.status(404).json({
       status: 'fail',
-      message: 'Invalid data sent!'
+      message: 'Tour not found'
     });
   }
-};
 
-exports.deleteTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const tour = await Tour.findByPk(id);
+  await tour.update(req.body, { validator: true }); // Set validator to true
 
-    if (!tour) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Tour not found'
-      });
+  res.status(200).json({
+    status: 'success',
+    data: { tour }
+  });
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const tour = await Tour.findByPk(id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found'
+    });
+  }
+
+  await tour.destroy();
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.findAll({
+    attributes: [
+      [sequelize.literal('UPPER(difficulty)'), 'difficulty'],
+      [sequelize.fn('COUNT', sequelize.col('id')), 'numTours'],
+      [sequelize.fn('SUM', sequelize.col('ratingsQuantity')), 'numRatings'],
+      [sequelize.fn('AVG', sequelize.col('ratingsAverage')), 'avgRating'],
+      [sequelize.fn('AVG', sequelize.col('price')), 'avgPrice'],
+      [sequelize.fn('MIN', sequelize.col('price')), 'minPrice'],
+      [sequelize.fn('MAX', sequelize.col('price')), 'maxPrice']
+    ],
+    where: {
+      ratingsAverage: { [Op.gte]: 4.5 },
+      secretTour: false
+    },
+    group: [sequelize.literal('UPPER(difficulty)')],
+    order: [[sequelize.col('avgPrice'), 'ASC']]
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats
     }
+  });
+});
 
-    await tour.destroy();
-
-    res.status(204).json({
-      status: 'success',
-      data: null
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error'
-    });
-  }
-};
-
-exports.getTourStats = async (req, res, next) => {
-  try {
-    const stats = await Tour.findAll({
-      attributes: [
-        [sequelize.literal('UPPER(difficulty)'), 'difficulty'],
-        [sequelize.fn('COUNT', sequelize.col('id')), 'numTours'],
-        [sequelize.fn('SUM', sequelize.col('ratingsQuantity')), 'numRatings'],
-        [sequelize.fn('AVG', sequelize.col('ratingsAverage')), 'avgRating'],
-        [sequelize.fn('AVG', sequelize.col('price')), 'avgPrice'],
-        [sequelize.fn('MIN', sequelize.col('price')), 'minPrice'],
-        [sequelize.fn('MAX', sequelize.col('price')), 'maxPrice']
-      ],
-      where: {
-        ratingsAverage: { [Op.gte]: 4.5 },
-        secretTour: false
-      },
-      group: [sequelize.literal('UPPER(difficulty)')],
-      order: [[sequelize.col('avgPrice'), 'ASC']]
-    });
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats
-      }
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error'
-    });
-  }
-};
-
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const plan = await sequelize.query(
-      `
-      SELECT EXTRACT(MONTH FROM "startDates") AS "month", COUNT("id") AS "numTourStarts", SUM("price") AS "totalRevenue", ARRAY_AGG("name") AS "tourNames"
-  FROM (
-    SELECT unnest("startDates") AS "startDates", "id", "price", "name"
-    FROM "Tours"
-    WHERE "secretTour" = false
-  ) AS "unnestedTours"
-  WHERE EXTRACT(YEAR FROM "startDates") = :year
-  GROUP BY "month"
-  ORDER BY "month" ASC
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const plan = await sequelize.query(
+    `
+    SELECT EXTRACT(MONTH FROM "startDates") AS "month", COUNT("id") AS "numTourStarts", SUM("price") AS "totalRevenue", ARRAY_AGG("name") AS "tourNames"
+    FROM (
+      SELECT unnest("startDates") AS "startDates", "id", "price", "name"
+      FROM "Tours"
+      WHERE "secretTour" = false
+    ) AS "unnestedTours"
+    WHERE EXTRACT(YEAR FROM "startDates") = :year
+    GROUP BY "month"
+    ORDER BY "month" ASC
     `,
-      {
-        replacements: { year: req.params.year },
-        type: QueryTypes.SELECT
-      }
-    );
+    {
+      replacements: { year: req.params.year },
+      type: QueryTypes.SELECT
+    }
+  );
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        plan
-      }
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Internal server error'
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      plan
+    }
+  });
+});

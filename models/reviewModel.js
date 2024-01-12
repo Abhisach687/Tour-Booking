e13ea6976844f71038aa192744f1b48dabddefff
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-12 14:10:00 +0545

    review Model

---
+++ models/reviewModel.js
@@ -0,0 +1,52 @@
+const { sequelize, Sequelize } = require('../db');
+const Tour = require('./tourModel');
+const User = require('./userModel');
+
+const Review = sequelize.define(
+  'Review',
+  {
+    review: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false
+    },
+    rating: {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    },
+    createdAt: {
+      type: Sequelize.DataTypes.DATE,
+      defaultValue: Sequelize.NOW
+    },
+    tourId: {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Tour,
+        key: 'id'
+      }
+    },
+    userId: {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id'
+      }
+    }
+  },
+  {
+    tableName: 'reviews',
+    timestamps: false
+  }
+);
+
+Review.belongsTo(Tour, { foreignKey: 'tourId' });
+Tour.hasMany(Review, { foreignKey: 'tourId' });
+
+Review.sync();
+
+module.exports = Review;

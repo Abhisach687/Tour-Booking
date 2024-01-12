8fc11297f57c90cd84617478c8eff3ddef9eb2d8
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-12 15:51:30 +0545

    Nested getAll Reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,11 @@ const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.findAll();
+  let filter = {};
+  if (req.params.tourId) filter = { tourId: req.params.tourId };
+  else if (req.user) filter = { userId: req.user.id };
+
+  const reviews = await Review.findAll({ where: filter });
 
   res.status(200).json({
     status: 'success',

aa0797558e18d18022037eea27d56f4ba2b0875f
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-13 16:45:44 +0545

    set up pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -12,7 +13,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(helmet());
@@ -53,9 +60,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   // middleware
   req.requestTime = new Date().toISOString();
@@ -63,7 +67,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-
+app.use('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
